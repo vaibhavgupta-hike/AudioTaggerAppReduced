4394a1bff4c33ed4c07512af8a2bd7eda6882085
vaibhavgupta <vaibhavg@hike.in>
2020-09-07 20:50:42 +0530

    The main activity now shows the nav host fragment by default

--- app/src/main/java/com/hike/tagging/audiotaggerappreduced/MainActivity.kt
+++ app/src/main/java/com/hike/tagging/audiotaggerappreduced/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.hike.tagging.audiotaggerappreduced
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         setContentView(R.layout.activity_main)
     }
 }
