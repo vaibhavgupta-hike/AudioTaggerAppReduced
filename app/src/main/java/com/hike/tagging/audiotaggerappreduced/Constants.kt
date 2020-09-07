cb0597eb03e01736679a8703eb49b1822acf308e
vaibhavgupta <vaibhavg@hike.in>
2020-09-07 19:56:05 +0530

    Added new Constants file

---
+++ app/src/main/java/com/hike/tagging/audiotaggerappreduced/Constants.kt
@@ -0,0 +1,8 @@
+package com.hike.tagging.audiotaggerappreduced
+
+object Constants {
+    const val INTERNET_PERMISSION_CODE = 1001
+    const val READ_EXT_STORAGE_PERMISSION_CODE = 1002
+    const val WRITE_EXT_STORAGE_PERMISSION_CODE = 1003
+    const val RECORD_AUDIO_PERMISSION_CODE = 1004
+}
