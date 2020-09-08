8373e9cd4359bd25b9729946fab29e42cf031743
vaibhavgupta <vaibhavg@hike.in>
2020-09-08 12:13:37 +0530

    DefaultAuthToken is the correct name for this variable

--- app/src/main/java/com/hike/tagging/audiotaggerappreduced/utils/AuthenticationUtils.kt
+++ app/src/main/java/com/hike/tagging/audiotaggerappreduced/utils/AuthenticationUtils.kt
@@ -7,11 +7,11 @@ object AuthenticationUtils {
     var googleAccount: GoogleSignInAccount? = null
     var clientToken: String? = null
 
-    val defaultClientToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZhaWJoYXZnQGhpa2UuaW4iLCJpYXQiOjE1OTkwMzY4MzB9.xNs1NOREAqh8ERK_wMJKvaH-IGvQOztCt2mrtWm5rOw"
+    val defaultAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZhaWJoYXZnQGhpa2UuaW4iLCJpYXQiOjE1OTgyNjQwODZ9.0vCG8fCL-lXfFDvmrSKYv6CEVXVbj_G3cJ5Npur-gCw"
     val defaultEmail = "vaibhavg@hike.in"
 
     fun getAuthToken(): String {
-        return (googleAccount?.idToken) ?: defaultClientToken
+        return (googleAccount?.idToken) ?: defaultAuthToken
     }
 
     fun getEmail(): String {
