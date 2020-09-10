6a3e5be45061926e670019b91c45c98ff05783b7
vaibhavgupta <vaibhavg@hike.in>
2020-09-10 13:57:07 +0530

    Enum class - RecordButtonStates to hold all the relevant button states for record button

---
+++ app/src/main/java/com/hike/tagging/audiotaggerappreduced/recorder/RecordButtonStates.kt
@@ -0,0 +1,9 @@
+package com.hike.tagging.audiotaggerappreduced.recorder
+
+enum class RecordButtonStates {
+    NOT_RECORDING,
+    RECORDING,
+    RECORD_FAILED,
+    UPLOADING,
+    UPLOAD_FAIL
+}
