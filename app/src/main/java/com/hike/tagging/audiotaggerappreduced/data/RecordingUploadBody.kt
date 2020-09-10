057a0b3c57e3b503ca1c68e75824bbc06d1bcebb
vaibhavgupta <vaibhavg@hike.in>
2020-09-10 12:04:56 +0530

    The class RecordingUploadBody only accepts audio as that is the only parameter needed now

--- app/src/main/java/com/hike/tagging/audiotaggerappreduced/data/RecordingUploadBody.kt
+++ app/src/main/java/com/hike/tagging/audiotaggerappreduced/data/RecordingUploadBody.kt
@@ -6,11 +6,11 @@ import java.io.File
 class RecordingUploadBody {
     private lateinit var audio: String
 
-    constructor(username: String, questionId: String, text: String, audio: String) {
+    constructor(audio: String) {
         this.audio = audio
     }
 
-    constructor(username: String, questionId: String, text: String, audio: File) {
+    constructor(audio: File) {
         val audioBase64Bytes = audio.inputStream().readBytes()
         val audioBase64Str = "data:audio/wav;base64," + Base64.encodeToString(audioBase64Bytes, 0)
         this.audio = audioBase64Str
